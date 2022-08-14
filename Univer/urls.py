dac4988efd9e0f1cafea86af16ee7fda20c97b85
anto777molina777@gmail.com <anto777molina777@gmail.com>
2022-08-14 16:45:03 -0500

    venv

--- Univer/urls.py
+++ Univer/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', list_Uni),
     path('new_task/', create_task, name='create_task'),
     path('delete_task/<int:task_id>/', delete_task, name='delete_task'),
-    path('editar_task/<int:task_id>/', editar_task, name='editar_task')
+    path('editar_task/<int:task:id/', editar_task, name='editar_task' )
 ]
--- Univer/views.py
+++ Univer/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render,redirect
 from .models import Task
-from .forms import TaskForm
+
 
 # Create your views here.
 def list_Uni(request):
     Univer = Task.objects.all()
+    
     return render(request, 'list_Uni.html', {"Univer": Univer})
 
 def create_task(request):
-    task=Task(decano=request.POST['decano'], apellido=request.POST['apellido'], titulo=request.POST['titulo'], descripcion=request.POST['descripcion'], direccion=request.POST['direccion'])
+    task=Task(decano=request.POST['decano'], 
+              apellido=request.POST['apellido'], 
+              titulo=request.POST['titulo'], 
+              descripcion=request.POST['descripcion'], 
+              direccion=request.POST['direccion'])
     task.save()
     return redirect('/Univer/')
 
@@ -28,4 +33,3 @@ def editar_task(request,task_id):
    
    task.save()
    return redirect("/Univer/")
-   
